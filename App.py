170e16aa048d2ad461ed245c8ca7e067c4c409d8
Callum Chalifour <callumblair42@gmail.com>
2024-06-07 13:43:22 -0700

    Add api request

--- App.py
+++ App.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from Keys import apiKey
+import requests
+
+apiLink = "https://ssd-api.jpl.nasa.gov/fireball.api?limit=20"
 
 def create_app():
     app = Flask("abc")
 
     @app.route('/')
-    def hello_world():
-        return 'Hello, World!'
+    def Home():
+        response = requests.get(apiLink)
+        response_json = response.json()
+        return response_json
     return app
 app = create_app
